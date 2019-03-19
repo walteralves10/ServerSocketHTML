67be85d811ae6f6b12685658fcbf07bb0b9132ec
walteralves10 <walteralves27@gmail.com>
2019-03-19 10:15:49 -0300

    modificando para os padrões walter

--- src/ServidorHtml.java
+++ src/ServidorHtml.java
@@ -1,92 +1,62 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
 
 public class ServidorHtml {
 
     public static void main(String[] args) throws IOException {
-        /* cria um socket "servidor" associado a porta 8000
-         já aguardando conexões
-         */
-        ServerSocket servidor = new ServerSocket(8000);
-        //aceita a primeita conexao que vier
+        
+        ServerSocket servidor = new ServerSocket(8530);
         Socket socket = servidor.accept();
-        //verifica se esta conectado
+        
         if (socket.isConnected()) {
-            //imprime na tela o IP do cliente
+            
             System.out.println(socket.getInetAddress());
-            //cria um BufferedReader a partir do InputStream do cliente
+            
             BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println("Requisição: ");
-            /* Lê a primeira linha
-             contem as informaçoes da requisição
-             */
+            
             String linha = buffer.readLine();
-            //quebra a string pelo espaço em branco
+            System.out.println(linha);
+            
             String[] dadosReq = linha.split(" ");
-            //pega o metodo
-            String metodo = dadosReq[0];
-            //paga o caminho do arquivo
-            String caminhoArquivo = dadosReq[1];
-            //pega o protocolo
+            String localArquivo = dadosReq[1];
             String protocolo = dadosReq[2];
-            //Enquanto a linha não for vazia
-            while (!linha.isEmpty()) {
-                //imprime a linha
-                System.out.println(linha);
-                //lê a proxima linha
-                linha = buffer.readLine();
-            }
-            //se o caminho foi igual a / entao deve pegar o /index.html
-            if (caminhoArquivo.equals("/")) {
-                //caminhoArquivo = "index.html";
-                caminhoArquivo = "C:\\Users\\AGUIA\\Desktop\\html\\ServidorHtml\\src\\index.html";
-            }
-            //abre o arquivo pelo caminho
-            File arquivo = new File(caminhoArquivo.replaceFirst("/", ""));
-
+            
+            if (localArquivo.equals("/"))
+                localArquivo = "src\\index.html";
+            
+            File BuscaHtml = new File(localArquivo);
             String status = protocolo + " 200 OK\r\n";
-            //se o arquivo não existe então abrimos o arquivo de erro, e mudamos o status para 404
-            if (!arquivo.exists()) {
+            
+            if (!BuscaHtml.exists()) {
                 status = protocolo + " 404 Not Found\r\n";
-                arquivo = new File("404.html");
+                System.out.println("NÃO ACHOU O LOCAL DO ARQUIVO");
             }
 
-            //lê todo o conteúdo do arquivo para bytes
-            byte[] conteudo = Files.readAllBytes(arquivo.toPath());
-            //cria um formato para o GMT espeficicado pelo HTTP
-            SimpleDateFormat formatador = new SimpleDateFormat("E, dd MMM yyyy hh:mm:ss", Locale.ENGLISH);
-            formatador.setTimeZone(TimeZone.getTimeZone("GMT"));
+            byte[] conteudo = Files.readAllBytes(BuscaHtml.toPath());
+            
             Date data = new Date();
-            //Formata a dara para o padrao
-            String dataFormatada = formatador.format(data) + " GMT";
-            //cabeçalho padrão da resposta HTTP
+            
             String header = status
-                    + "Location: http://localhost:8000/\r\n"
-                    + "Date: " + dataFormatada + "\r\n"
+                    + "Location: http://localhost:8530/\r\n"
+                    + "Date: " + data + "\r\n"
                     + "Server: MeuServidor/1.0\r\n"
                     + "Content-Type: text/html\r\n"
                     + "Content-Length: " + conteudo.length + "\r\n"
                     + "Connection: close\r\n"
                     + "\r\n";
-            //cria o canal de resposta utilizando o outputStream
+            
             OutputStream resposta = socket.getOutputStream();
-            //escreve o headers em bytes
+            
             resposta.write(header.getBytes());
-            //escreve o conteudo em bytes
             resposta.write(conteudo);
-            //encerra a resposta
+            
             resposta.flush();
         }
     }
