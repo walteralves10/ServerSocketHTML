7819e8be1e39273fe675d10e2d34d6ca32b25ab1
walteralves10 <walteralves27@gmail.com>
2019-03-20 10:42:19 -0300

    deixando o servidor constante

--- src/servidorPaginaHtml/ServidorHtml.java
+++ src/servidorPaginaHtml/ServidorHtml.java
@@ -13,54 +13,67 @@ import java.util.Date;
 public class ServidorHtml {
 
     public static void main(String[] args) throws IOException {
-        
+
         ServerSocket servidor = new ServerSocket(8530);
         System.out.println("Aguardando conexao ...");
-        Socket socket = servidor.accept();
-        
-        if (socket.isConnected()) {
-            
-            System.out.println(socket.getInetAddress());
-            
+
+        //if (socket.isConnected()) {
+        while (true) {
+
+            Socket socket = servidor.accept();
+
+            System.out.println(socket.getRemoteSocketAddress());
+
             BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            
+
             String linha = buffer.readLine();
-            System.out.println(linha);
-            
-            String[] dadosReq = linha.split(" ");
-            String localArquivo = dadosReq[1];
-            String protocolo = dadosReq[2];
-            
-            if (localArquivo.equals("/"))
-                localArquivo = "index.html";
-            
-            File BuscaHtml = new File(localArquivo);
-            String status = protocolo + " 200 OK\r\n";
-            
-            if (!BuscaHtml.exists()) {
+            System.out.println(linha + "\n");
+            String[] divideRequisicao;
+            String localArquivo = null, protocolo = null, status = null;
+            File BuscaHtml = null;
+            byte[] conteudo = null;
+
+            if (linha != null) {
+                divideRequisicao = linha.split(" ");
+                localArquivo = divideRequisicao[1];
+                protocolo = divideRequisicao[2];
+
+                if (localArquivo.equals("/")) {
+                    localArquivo = "src\\servidorPaginaHtml\\index.html";
+                    BuscaHtml = new File(localArquivo);
+                    status = protocolo + " 200 OK\r\n";
+
+                    conteudo = Files.readAllBytes(BuscaHtml.toPath());
+
+                    Date data = new Date();
+
+                    String header = status
+                            + "Location: http://localhost:8530/\r\n"
+                            + "Date: " + data + "\r\n"
+                            + "Server: MeuServidor/1.0\r\n"
+                            + "Content-Type: text/html\r\n"
+                            + "Content-Length: " + conteudo.length + "\r\n"
+                            + "Connection: close\r\n"
+                            + "\r\n";
+
+                    OutputStream resposta = socket.getOutputStream();
+
+                    resposta.write(header.getBytes());
+                    resposta.write(conteudo);
+
+                    resposta.flush();
+                }
+            } else {
                 status = protocolo + " 404 Not Found\r\n";
                 System.out.println("NAO ACHOU O LOCAL DO ARQUIVO");
             }
 
-            byte[] conteudo = Files.readAllBytes(BuscaHtml.toPath());
-            
-            Date data = new Date();
-            
-            String header = status
-                    + "Location: http://localhost:8530/\r\n"
-                    + "Date: " + data + "\r\n"
-                    + "Server: MeuServidor/1.0\r\n"
-                    + "Content-Type: text/html\r\n"
-                    + "Content-Length: " + conteudo.length + "\r\n"
-                    + "Connection: close\r\n"
-                    + "\r\n";
-            
-            OutputStream resposta = socket.getOutputStream();
-            
-            resposta.write(header.getBytes());
-            resposta.write(conteudo);
-            
-            resposta.flush();
+            //if (!BuscaHtml.exists()) {
+            //    status = protocolo + " 404 Not Found\r\n";
+            //    System.out.println("NAO ACHOU O LOCAL DO ARQUIVO");
+            //}
         }
+
+        //}
     }
 }
